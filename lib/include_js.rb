3fc2629786569ee2ee6bee62b934813293050964
Andreas Haller <andreas@kopfmaschine.com>
2011-05-19 01:56:19 -0400

    Add IncludeJS::Env - An empty environment

--- lib/include_js.rb
+++ lib/include_js.rb
@@ -1,12 +1,24 @@
 require 'v8'
+require 'forwardable'
 
-module IncludeJS
-  @root_path = File.expand_path('.')
-  @modules = {} # This stores all loaded modules by their absolute path (not their id)
-  @cxt = V8::Context.new
-  
+module IncludeJS  
   class << self
+    extend Forwardable
+    def_delegators :instance, :require, :module, :root_path, :root_path=
+  end
+    
+  def self.instance
+    @instance ||= Env.new
+  end
+  
+  class Env
     attr_accessor :root_path # FIXME Make this act like a PATH (see Modules 1.1 require.paths) and think about the API
+    
+    def initialize
+      @root_path = File.expand_path('.')
+      @modules = {} # This stores all loaded modules by their absolute path (not their id)
+      @engine = V8::Context.new
+    end
     
     def require(module_id, globals={})
       load_module(module_id, globals, nil)
@@ -27,11 +39,11 @@ module IncludeJS
     def load_module(module_id, globals, caller_path)
       path = absolute_path(module_id, caller_path)
       return @modules[path] if @modules[path]
-      globals.each { |name, value| @cxt[name] = value } # FIXME Remove. globals is just hax to make the tests pass
-      exports = @modules[path] = @cxt['Object'].new
+      globals.each { |name, value| @engine[name] = value } # FIXME Remove. globals is just hax to make the tests pass
+      exports = @modules[path] = @engine['Object'].new
       require_fn = lambda { |module_id| load_module(module_id, globals, path) }
       
-      context = @cxt.eval("(function(exports, require){ #{File.read(path)}})")
+      context = @engine.eval("(function(exports, require){ #{File.read(path)}})")
       context.call(exports, require_fn)
       exports
     end
